7668f268d498fe48862da3457f2c9422c17a743a
Gabriel Fuentes <gffuentes@gmail.com>
2014-09-27 11:25:28 -0400

    adding some modifications to the socket.io stuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :room
+	after_create {|comment| comment.message 'create' }
+  after_update {|comment| comment.message 'update' }
+  after_destroy {|comment| comment.message 'destroy' }
+
+	  def message action
+    msg = { resource: 'comments',
+            action: action,
+            id: self.id,
+            obj: self }
+
+    $redis.publish 'rt-change', msg.to_json
+  end
 end
