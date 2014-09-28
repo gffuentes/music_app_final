c93a4a6e2400ac46aa89a11d654c74a82405b807
Gabriel Fuentes <gffuentes@gmail.com>
2014-09-28 00:38:29 -0400

    adding all the sockets stuff before i get rid of it

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -1,10 +1,24 @@
-var io = require('socket.io').listen(5001),
-    redis = require('redis').createClient();
-
-redis.subscribe('rt-change');
-
+var http = require('http').Server()
+var io = require('socket.io')(http);
 io.on('connection', function(socket){
-  redis.on('message', function(channel, message){
-    socket.emit('rt-change', JSON.parse(message));
-  });
+  console.log('a user connected');
 });
+var redis = require('redis'),
+	client = redis.createClient();
+var io2 = require('socket.io-emitter')({host:'localhost',port:'6379'});
+setInterval(function(){
+  io2.emit('time', 'Date');
+}, 5001);
+var socket = io2.of('/thing')
+socket.emit('testing','test please')
+
+// client.subscribe('rt-change');
+
+
+// io.on('connection', function(socket){
+//   redis.on('message', function(channel, message){
+//     socket.emit('rt-change', JSON.parse(message));
+//   });
+// });
+
+// client.subscribe('rt-change','testing');
