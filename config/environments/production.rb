93aec1c0438a22168a32efda44a1b109c9ebbb83
Gabriel Fuentes <gffuentes@gmail.com>
2014-09-30 01:03:44 -0400

    try compiling again i guess 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,11 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.css )
+  config.assets.precompile += ['*.css']
+  config.assets.precompile += ['*.js']
+  config.assets.precompile += ['*.html']
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
